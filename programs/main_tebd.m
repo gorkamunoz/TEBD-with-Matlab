4d85b64949131a35d6b2d3b7cb4ef30128d558bd
gorkamunoz <gorkamunoz@users.noreply.github.com>
2017-08-03 15:38:25 +0200

    Update main_tebd.m

--- programs/main_tebd.m
+++ programs/main_tebd.m
@@ -14,22 +14,14 @@ t_max = 200;
 MPS = initial_mps(N, d, D);
 
 %% Hamiltonian (needs to be defined inside functions)
-% 
-% sx = [0 1; 1 0];
-% sz = [1 0; 0 -1];
-% I=eye(2);
-% 
-% g_prime = 0; % This should be include in the for of the mps sites
-% 
+
 % H = -J*(kron(sz,sz)+g*kron(sx,I)+g_prime*kron(I,sx));
 % due to kronecker delta inverting the order of the operators, kron(sx, I)
 % means Ixsx, so we are applying sx to the second site. This means that we
 % need the term in g_prime to get the magnetic in the first site! ie
 % g_prime = g for site 1 and 0 for all the others -> the H has to be
 % defined inside the MPS
-% 
-% 
-% exp_operator = expm(-tau*H);
+
 
 %% Evolution 
 energy = zeros(1, t_max);
