8082bb82aec1df7a8e27af451e20233fddb99e67
Unknown <gorkam@hotmail.com>
2017-10-10 18:39:05 +0200

    Borrado todo lo antiguo

--- programs/contraction.m
+++
@@ -1,3 +0,0 @@
-function D = contraction(A, B, C)
-
-D = ncon({A, B, C},{[1 2], [3 1 -1], [3 2 -2]}, [1 2 3]);
--- programs/theoretical_ising.m
+++
@@ -1,6 +0,0 @@
-function q = theoretical_ising(J, g)
-
-ising = @(x)(-1/(4*pi))*2*J*sqrt(1+g^2-2*g*cos(x));
-% ising = @(x)(-1/(4*pi))*(J/2)*sqrt(1/4+g^2-g*cos(x));
-
-q = integral(ising,-pi,pi);
